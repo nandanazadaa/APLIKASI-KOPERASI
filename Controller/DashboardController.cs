8474b581276f2612ea8c8932b54dbfd7c97d49c0
Nandana Zada Abiproya <95664499+nandanazadaa@users.noreply.github.com>
2024-12-10 14:46:39 +0700

    Add files via upload

---
+++ Controller/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿ausing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APLIKASI_KOPERASI.Controller
+{
+    internal class DashboardController
+    {
+    }
+}
